61c629f26118a932141f7e52acd4dd89ec7016a1
zk8080 <zhangkang8080@163.com>
2019-07-19 18:47:05 +0800

    修改错误

--- src/pubComponents/index.js
+++ src/pubComponents/index.js
@@ -1,6 +1,6 @@
 import Table from './Table/index.component';
 import Modal from './Modal/index.component';
-import DownLoad from './downLoad/index.component';
+import DownLoad from './DownLoad/index.component';
 import Select from './Select/index.component';
 import EditTable from './EditTable/index.component';
 import BarcodeComponent from './barcode/barcode.component';
