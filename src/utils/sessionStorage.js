0a01907612cbfb899b4687bf5eddf9561db068ad
zk8080 <zhangkang8080@163.com>
2019-07-22 18:04:53 +0800

    增加setcookie方法

--- src/utils/sessionStorage.js
+++ src/utils/sessionStorage.js
@@ -18,6 +18,14 @@ class SessionStorage {
             console.log(e);
         }
     }
+
+    // 设置cookie
+    setCookie = (name,value) => {
+        var Days = 30;
+        var exp = new Date();
+        exp.setTime(exp.getTime() + Days*24*60*60*1000);
+        document.cookie = name + '='+ escape (value) + ';expires=' + exp.toGMTString();
+    }
 }
 
 export default new SessionStorage();
