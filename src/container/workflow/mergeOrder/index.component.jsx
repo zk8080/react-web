077a8aa0b50f962afddecf484a9e22f6da61e474
zk8080 <zhangkang8080@163.com>
2019-08-27 22:25:43 +0800

    完善逻辑

--- src/container/workflow/mergeOrder/index.component.jsx
+++ src/container/workflow/mergeOrder/index.component.jsx
@@ -5,7 +5,7 @@ import {Table} from '@pubComs';
 import {colums} from './index.data';
 import './index.less';
 import { toJS } from 'mobx';
-import {Button} from 'antd';
+import {Button, message} from 'antd';
 
 @observer
 class Index extends Component {
@@ -39,6 +39,31 @@ class Index extends Component {
 
     mergeOrder = () => {
         const {selectedRows} = this.state;
+        if(selectedRows.length < 1){
+            message.warning('请选择要合单的数据！');
+            return;
+        }
+        for (let i = 0; i < selectedRows.length; i++) {
+            for (let j = i; j < selectedRows.length; j++) {
+                if( selectedRows[i].customerName != selectedRows[j].customerName ){
+                    message.warning('选择的订单中商家不同！');
+                    return;
+                }
+                if( selectedRows[i].reciptName != selectedRows[j].reciptName ){
+                    message.warning('选择的订单中收件人不同！');
+                    return;
+                }
+                if( selectedRows[i].reciptPhone != selectedRows[j].reciptPhone ){
+                    message.warning('选择的订单中手机号不同！');
+                    return;
+                }
+                if( selectedRows[i].reciptAddr != selectedRows[j].reciptAddr ){
+                    message.warning('选择的订单中地址不同！');
+                    return;
+                }
+            }
+            
+        }
         const orderNoList = selectedRows.map(item => item.systemOrderNo);
         State.mergeOrder( {systemOrderNos:orderNoList});
     }
@@ -46,8 +71,8 @@ class Index extends Component {
     render() {
         this.rowSelection.selectedRowKeys = this.state.selectedRowKeys;
         return (
-            <div>
-                <div>
+            <div className='merge_order'> 
+                <div className='head_btn'>
                     <Button
                         type='primary'
                         onClick={this.mergeOrder}
